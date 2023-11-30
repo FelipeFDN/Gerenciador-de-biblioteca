4eb3781ee2f001532ec09c3c387bf0fdcf52e731
FelipeFDN <felipednerling@gmail.com>
2023-11-29 23:35:54 -0300

    Inicio do BackEnd

---
+++ BackEnd/biblioteca/src/main/java/com/biblioteca/biblioteca/entities/Livro.java
@@ -0,0 +1,28 @@
+package main.java.com.biblioteca.biblioteca.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Livro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    @ManyToOne
+
+    String titulo;
+    Usuario user;
+    int disponivel;
+    LocalDate data;
+}
---
+++ BackEnd/biblioteca/src/main/java/com/biblioteca/biblioteca/entities/Usuario.java
@@ -0,0 +1,26 @@
+package main.java.com.biblioteca.biblioteca.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String nome;
+    String cpf;
+    String telefone;
+}
